09a3f41e72edf26fccbaef3fb5ec9db09654308f
Hassam97 <hassam.pixcile@gmail.com>
2026-07-20 20:33:52 +0500

    Increase payload limit to 50mb

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ app.use(cors({
   methods: ['GET', 'POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type']
 }));
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Tracker backend is running!' });
